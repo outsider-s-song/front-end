beab1e23212a9a763202f287a3eeb080b7be6135
kyuung <mel1521@naver.com>
2021-12-25 12:52:34 +0900

    ✅  Add tone

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { add, minus } from './redux/modules/score';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
+// * tone.js sample
+import * as Tone from 'tone';
+
 const App = () => {
+	const synth = new Tone.Synth().toDestination();
+	const getNote = () => {
+		synth.triggerAttackRelease('C4', '8n');
+	};
 	const dispatch = useDispatch();
 	const number = useSelector((state) => state.count.number);
+
 	return (
 		<>
+			<h1>tone</h1>
+			<button onClick={getNote}>getNote</button>
+			<br />
 			<h1>{number}</h1>
 			<button onClick={() => dispatch(add())}>plus</button>
 			<button onClick={() => dispatch(minus())}>minus</button>
